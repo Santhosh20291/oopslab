a5628f9015f5b4ce90c01b375dd756305d84ef27
Santhosh20291 <159427675+Santhosh20291@users.noreply.github.com>
2024-02-17 19:47:51 +0530

    Create manipulators.cpp

---
+++ manipulators.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"USING '\\n' ...\n";
+	cout<<"Line 1 \nLine 2 \nLine 3 \n";
+	cout<<"USING end ..."<< endl;
+	cout<<"Line 1"<<endl<<"Line 2"<<endl<<"Line 3"<<endl;
+}
