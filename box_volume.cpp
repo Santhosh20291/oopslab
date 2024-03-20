8cace0ad97aa5cea0eaf3aa5943f583d257379e1
Santhosh20291 <159427675+Santhosh20291@users.noreply.github.com>
2024-03-20 19:20:07 +0530

    Create box_volume.cpp

---
+++ box_volume.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+float Volume;                                                         
+void boxVolume(float length,float width,float height){
+    Volume = length*width*height;
+    cout<<"the volume of the box: "<<Volume<<endl;
+}                                                                                          
